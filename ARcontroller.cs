3c6b877c1b99ef0c130cc0769d8ea470955f025e
xrigiitm <158250458+xrigiitm@users.noreply.github.com>
2024-02-03 22:36:39 +0530

    Add files via upload
    This Unity script, ARController, is designed for use with AR Foundation to manage and respond to changes in tracked images within an augmented reality (AR) scene. It primarily focuses on handling events when AR images are detected, updated, or removed from the scene. Upon detecting a new tracked image, the script dynamically assigns a specified prefab as a child to the detected image's transform, allowing developers to easily associate 3D objects or UI elements with specific real-world images recognized by the AR session. This functionality is crucial for creating interactive AR experiences that overlay digital content on real-world images, such as educational tools, gaming, and marketing applications. The script ensures a seamless integration of digital content with physical spaces, enhancing user engagement and immersion in AR environments.

---
+++ ARcontroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARController : MonoBehaviour
+{
+    [SerializeField] ARTrackedImageManager m_TrackedImageManager;
+    [SerializeField] Transform prefab;
+    void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
+
+    void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var newImage in eventArgs.added)
+        {
+
+            prefab.parent = newImage.gameObject.transform;
+            prefab.transform.localPosition = new Vector3(0f, 0f, 0f);
+        }
+
+        foreach (var updatedImage in eventArgs.updated)
+        {
+            // Handle updated event
+        }
+
+        foreach (var removedImage in eventArgs.removed)
+        {
+            // Handle removed event
+        }
+    }
+}
