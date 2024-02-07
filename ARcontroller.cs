6b3ed2ba9915a3b1bc87e79fdebc5a1ecb7b5793
/CODER <54804241+Maapel@users.noreply.github.com>
2024-02-07 10:49:55 +0530

    Update ARcontroller.cs

--- ARcontroller.cs
+++ ARcontroller.cs
@@ -7,7 +7,7 @@ using UnityEngine.XR.ARFoundation;
 public class ARController : MonoBehaviour
 {
     [SerializeField] ARTrackedImageManager m_TrackedImageManager;
-    [SerializeField] Transform prefab;
+    [SerializeField] Transform objectToBePlaced;
     void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
 
     void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
@@ -17,13 +17,14 @@ public class ARController : MonoBehaviour
         foreach (var newImage in eventArgs.added)
         {
 
-            prefab.parent = newImage.gameObject.transform;
-            prefab.transform.localPosition = new Vector3(0f, 0f, 0f);
+            objectToBePlaced.parent = newImage.gameObject.transform;
+            objectToBePlaced.transform.localPosition = new Vector3(0f, 0f, 0f);
         }
 
         foreach (var updatedImage in eventArgs.updated)
         {
-            // Handle updated event
+            objectToBePlaced.parent = newImage.gameObject.transform;
+            objectToBePlaced.transform.localPosition = new Vector3(0f, 0f, 0f);
         }
 
         foreach (var removedImage in eventArgs.removed)
